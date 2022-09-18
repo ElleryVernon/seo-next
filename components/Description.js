44e38dbf83783ce14424e7a3d2964fcc04382d26
ElleryVernon <tmpks3@gmail.com>
2022-09-19 00:28:34 +0900

    first commit

--- components/Description.js
+++ components/Description.js
@@ -3,7 +3,7 @@ import React from "react";
 const Description = () => {
 	return (
 		<main className="flex justify-center p-16 ">
-			<p className="text-gray-800 text-[2em] font-bold">
+			<p className="text-blue-800 text-[2em] font-bold">
 				누구나 이해할 수 있는 SEO 단순화를 목표로 합니다.
 			</p>
 		</main>
