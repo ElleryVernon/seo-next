7c012042be02870359ced280da0e09f450d85a60
ElleryVernon <tmpks3@gmail.com>
2022-10-01 22:24:28 +0900

    first commit

--- pages/api/test.js
+++ pages/api/test.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
 			`https://www.google.com/search?q=site:${encodeURI(rootDomain)}`,
 			AXIOS_OPTIONS
 		);
+		res.status(200).json({ message: "성공" });
 		const $ = cheerio.load(data);
 		const resultCount = Number(
 			$("#result-stats")
@@ -31,7 +32,6 @@ export default async function handler(req, res) {
 				.split("개")[0]
 				.replaceAll(",", "")
 		);
-		res.status(200).json({ message: "성공" });
 
 		res.status(200).json({
 			root: rootDomain,
